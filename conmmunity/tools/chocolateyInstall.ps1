8a6e7066bcfd7ea38d23837371d7cf9a3a2e28c2
Zed <zed@zedd.dev>
2020-10-06 15:30:00 +0800

    move from old repo

---
+++ conmmunity/tools/chocolateyInstall.ps1
@@ -0,0 +1,50 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$softwareName = 'IntelliJ IDEA Community Edition*'
+[array]$key = Get-UninstallRegistryKey -SoftwareName $softwareName
+
+if ($key.Count -gt 0) {
+    Invoke-Expression -Command $PSScriptRoot\chocolateyUninstall.ps1
+}
+
+$url = 'https://download.jetbrains.com/idea/ideaIC-2020.2.2.exe'
+$sha256sum = 'ea403c8926210f08e484752c9ed30056c8bd672ba8a0c07b7ad0bbd54a17c70f'
+
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+if ([System.Environment]::Is64BitOperatingSystem) {
+    $programFiles = $env:ProgramFiles
+}
+else {
+    $programFiles = ${env:ProgramFiles(x86)}
+}
+$installDir = "$programFiles\JetBrains\IntelliJ IDEA Community Edition $env:ChocolateyPackageVersion"
+
+$pp = Get-PackageParameters
+if ($pp.InstallDir) {
+    $installDir = $pp.InstallDir
+}
+
+$silentArgs = "/S /CONFIG=$toolsDir\silent.config "
+$silentArgs += "/D=$installDir"
+
+New-Item -ItemType Directory -Force -Path $installDir
+
+$packageArgs = @{
+    packageName    = $env:ChocolateyPackageName
+    unzipLocation  = $toolsDir
+    fileType       = 'exe'
+    url            = $url
+    url64bit       = $url
+
+    softwareName   = $softwareName
+
+    checksum       = $sha256sum
+    checksumType   = 'sha256'
+    checksum64     = $sha256sum
+    checksumType64 = 'sha256'
+
+    silentArgs     = $silentArgs
+    validExitCodes = @(0, 1641, 3010)
+}
+
+Install-ChocolateyPackage @packageArgs
---
+++ ultimate/tools/chocolateyInstall.ps1
@@ -0,0 +1,52 @@
+﻿$ErrorActionPreference = 'Stop';
+
+$softwareName = 'IntelliJ IDEA*'
+[array]$key = Get-UninstallRegistryKey -SoftwareName $softwareName
+
+if ($key.Count -gt 0) {
+    Invoke-Expression -Command $PSScriptRoot\chocolateyUninstall.ps1
+}
+
+$url = 'https://download.jetbrains.com/idea/ideaIU-2020.2.2.exe'
+$sha256sum = '0bf78a5e5e1305aaf17c4a2671cf9768228f9142775715ef19994a455b6c3da2'
+
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+if ([System.Environment]::Is64BitOperatingSystem) {
+    $programFiles = $env:ProgramFiles
+} else {
+    $programFiles = ${env:ProgramFiles(x86)}
+}
+
+$installDir = "$programFiles\JetBrains\IntelliJ IDEA $env:ChocolateyPackageVersion"
+
+$pp = Get-PackageParameters
+if ($pp.InstallDir) {
+    $installDir = $pp.InstallDir
+}
+
+
+
+$silentArgs = "/S /CONFIG=$toolsDir\silent.config "
+$silentArgs += "/D=$installDir"
+
+New-Item -ItemType Directory -Force -Path $installDir
+
+$packageArgs = @{
+    packageName    = $env:ChocolateyPackageName
+    unzipLocation  = $toolsDir
+    fileType       = 'exe'
+    url            = $url
+    url64bit       = $url
+
+    softwareName   = $softwareName
+
+    checksum       = $sha256sum
+    checksumType   = 'sha256'
+    checksum64     = $sha256sum
+    checksumType64 = 'sha256'
+
+    silentArgs     = $silentArgs
+    validExitCodes = @(0, 1641, 3010)
+}
+
+Install-ChocolateyPackage @packageArgs
