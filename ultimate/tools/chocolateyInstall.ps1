d880aa876e814829ac5a777fec15b760cfe13c3a
Roland Weisleder <roland.weisleder@googlemail.com>
2022-01-31 21:51:47 +0100

    Upgrade to 2021.3.2

--- ultimate/tools/chocolateyInstall.ps1
+++ ultimate/tools/chocolateyInstall.ps1
@@ -7,8 +7,8 @@ if ($key.Count -gt 0) {
     Invoke-Expression -Command $PSScriptRoot\chocolateyUninstall.ps1
 }
 
-$url = 'https://download.jetbrains.com/idea/ideaIU-2021.3.1.exe'
-$sha256sum = 'a048dbe07e93c4af0ea7d9ba6a2afedbd2ffd90441ddfa00049d2649b4541133'
+$url = 'https://download.jetbrains.com/idea/ideaIU-2021.3.2.exe'
+$sha256sum = 'd41c97d9f5149adae12ebac0dd98e7e789a07f3c0b1537379256eb55458a3c2d'
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 if ([System.Environment]::Is64BitOperatingSystem) {
