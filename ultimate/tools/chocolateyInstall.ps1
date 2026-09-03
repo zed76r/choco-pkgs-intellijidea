979d29b3ef5493ff07245184f1956b2e4e882b81
Zed <zed@zedd.dev>
2026-09-03 10:59:35 +0800

    chore: bump intellijidea packages to 2026.2.2

--- ultimate/tools/chocolateyInstall.ps1
+++ ultimate/tools/chocolateyInstall.ps1
@@ -7,10 +7,10 @@ if ($key.Count -gt 0) {
     Invoke-Expression -Command $PSScriptRoot\chocolateyUninstall.ps1
 }
 
-$url = 'https://download.jetbrains.com/idea/idea-2026.2.1.exe'
+$url = 'https://download.jetbrains.com/idea/idea-2026.2.2.exe'
 # how to get sha256sum
 # curl -s https://download.jetbrains.com/idea/idea-2025.3.3.exe.sha256 | awk '{print $1}' ORS="" | pbcopy
-$sha256sum = '83278d24439e582ab1af7965a3c3f894e64b43e3f1bdf0c2d659d4e23f790a57'
+$sha256sum = '692855b1b85f6d9291cf2fa78c95f3dc331316e150506c300d6860bd7f6f3907'
 
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 if ([System.Environment]::Is64BitOperatingSystem) {
